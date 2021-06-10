5833f3dcf664d722f52d4f588602fd3b13cecc3c
Atin Gupta <atingupta2005@gmail.com>
2021-06-10 16:58:51 +0530

    -

---
+++ Hands-On/316-azure-service-bus-queue-dot-net-receiving-multiple-messages/ServiceBus-Queue-Receive/ServiceBus-Queue-Receive/Program.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ServiceBus_Queue_Receive
+{
+    class Program
+    {
+        private static string connection_string = "Endpoint=sb://nssbatinjune21.servicebus.windows.net/;SharedAccessKeyName=p1;SharedAccessKey=Z1NI0W2G3ZLQQMVQNNIfY+UkqcLI3lDP7aTUaaMTz9c=;EntityPath=queue1";
+        private static string queue_name = "queue1";
+        static async Task Main(string[] args)
+        {
+            ServiceBusClient _client = new ServiceBusClient(connection_string);
+
+            ServiceBusReceiver _receiver = _client.CreateReceiver(queue_name,new ServiceBusReceiverOptions() {ReceiveMode= ServiceBusReceiveMode.ReceiveAndDelete });
+
+            var _messages =  _receiver.ReceiveMessagesAsync(2);
+
+            foreach (var _message in _messages.Result)
+            {
+                Console.WriteLine($"The Sequence number is {_message.SequenceNumber}");
+                Console.WriteLine(_message.Body);
+                
+            }
+
+        }
+    }
+}
---
+++ Hands-On/326-azure-service-bus-topic-dot-net-receiving-messages/AzureServiceBus-Topic-Receive/AzureServiceBus-Topic-Receive/Program.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+
+namespace AzureServiceBus_Topic_Receive
+{
+    class Program
+    {
+        private static string connection_string = "Endpoint=sb://nssbatinjune21.servicebus.windows.net/;SharedAccessKeyName=p1;SharedAccessKey=KQcIqmzeuqCRpJCiiTLqnfFhS2SFTY3w4Ia/HScZNMM=;EntityPath=topic1";
+        private static string topic_name = "topic1";
+        private static string subscription_name = "subscription1";
+        static void Main(string[] args)
+        {
+            ServiceBusClient _client = new ServiceBusClient(connection_string);
+            ServiceBusReceiver _receiver = _client.CreateReceiver(topic_name, subscription_name, new ServiceBusReceiverOptions() { ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete });
+
+            var _messages = _receiver.ReceiveMessagesAsync(2);
+
+            foreach (var _message in _messages.Result)
+            {
+                Console.WriteLine($"The Sequence number is {_message.SequenceNumber}");
+                Console.WriteLine(_message.Body);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
